19aaaedc4bc79d8b43dda75964663ef44428f48f
xxBkxx <bkdixxon@gmail.com>
2017-08-27 09:49:38 -0400

    sercurecConnection

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
+var http    = require('http');
+var https   = require('https');
 var message_routes = require('./routes/message.routes');
 
 // var message 	   = require('./models/message');
@@ -24,6 +26,18 @@ app.use('*', function(req,res,next){
 	res.sendFile(indexFile);
 })
 
+
+var insecureConn = http.createServer(app).listen(3000);
+
+const OPTIONS = {
+	key:  fs.readFileSync('blast.key'),
+	cert: fs.readFileSync('blast.crt')
+};
+
+var secureConn = https.createServer(OPTIONS, app).listen(8443)
+
+
+
 mongoose.connect('mongodb://localhost/data/db/');
 
 var db = mongoose.connection;
@@ -33,10 +47,10 @@ db.once('open', function(){
 	console.log('connected to db at /data/db');
 });
 
-app.listen(PORT, function(){
-	console.log("server started on http:/\/localhost:8080");
-	console.log("press ctrl + c  to stop server");
-});
+// app.listen(PORT, function(){
+// 	console.log("server started on http:/\/localhost:8080");
+// 	console.log("press ctrl + c  to stop server");
+// });
 
 
 
